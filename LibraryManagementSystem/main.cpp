e13f9880da61271580aa545cadce734925e68f8b
Niroshan Sugirtharatnam <codeniro@gmail.com>
2020-08-06 14:59:44 -0400

    set application theme to a modern style

--- LibraryManagementSystem/main.cpp
+++ LibraryManagementSystem/main.cpp
@@ -1,11 +1,15 @@
 #include "application.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setStyle(QStyleFactory::create("Fusion"));
+
     Application w;
     w.show();
+
     return a.exec();
 }
